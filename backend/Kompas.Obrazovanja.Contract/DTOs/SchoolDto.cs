6ec8d07f772da6decd1446d67ee1ac0fe7d40678
Jan <janivancic111@gmail.com>
2025-05-09 23:09:56 +0200

    fix git changes

---
+++ backend/Kompas.Obrazovanja.Contract/DTOs/SchoolDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Kompas.Obrazovanja.Contract.DTOs
+{
+    public class SchoolDto
+    {
+        public int SkolaId { get; set; }
+        public string Naziv { get; set; } = null!;
+        public string Adresa { get; set; } = null!;
+        public string Opis { get; set; } = null!;
+        public string Grad { get; set; } = null!;
+    }
+}
